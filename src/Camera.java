81b3830e301e67b49143ca95c92195a90a78ad75
wallksss <andersoncsassakig@gmail.com>
2024-09-26 13:21:10 -0300

    Corrected gamma (transformed from linear space to gamma space)

--- src/Camera.java
+++ src/Camera.java
@@ -109,10 +109,20 @@ public class Camera {
         return new Vec3(Util.randomDouble() - 0.5, Util.randomDouble() - 0.5, 0);
     }
 
+    public static double linearToGamma(double linear_component) {
+        if(linear_component > 0)
+            return Math.sqrt(linear_component);
+        return 0;
+    }
+
     public static void writeColor(BufferedWriter out, Vec3 pixelColor) {
         double r = pixelColor.getX();
         double g = pixelColor.getY();
         double b = pixelColor.getZ();
+
+        r = linearToGamma(r);
+        g = linearToGamma(g);
+        b = linearToGamma(b);
 
         Interval intensity = new Interval(0.000, 0.999);
         int rbyte = (int)(256 * intensity.clamp(r));
